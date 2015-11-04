07d0d746f9a4609858b09106899a7f00ed6f8426
yattha <yattha@uw.edu>
2015-11-03 19:09:47 -0800

    last Tuesday

--- src/CodingTree.java
+++ src/CodingTree.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -13,7 +14,8 @@ import java.util.PriorityQueue;
 public class CodingTree {
 
 	Map<Character, String> codes;
-	List<Byte> bits; 
+	List<Byte> bits;
+	String bitString; 
 	String textString;
 	List<CharF> frequencies = new ArrayList<CharF>();
 	PriorityQueue<Node> nodeQueue;
@@ -24,193 +26,112 @@ public class CodingTree {
 	public CodingTree(String message) {
 		textString = message;
 		codes = new HashMap<Character, String>();
-		bits = new ArrayList<Byte>();
+		bitString = "";
 		nodeQueue = new PriorityQueue<Node>();
+		bits = new ArrayList<Byte>();
 		countCharFrequency();
 		generateTree();
 		generateCodes();
+		encode();
 	}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	private void encode() {
+		int len = textString.length(), curPos = 0;
+		while(len-- > 1) {
+			bitString += codes.get(textString.charAt(curPos++));			
+		}
+		
+		//convert string to byte array
+		
+		while(!bitString.isEmpty()){
+			String singleByte = "";
+			if(bitString.length() > 7) {
+				singleByte = bitString.substring(0, 8);
+				bitString = bitString.substring(8);
+			} else {
+				singleByte = bitString;
+				while(singleByte.length() < 9) singleByte += "0";
+				bitString =  "";
+			}
+			byte tempB = 0;
+			for(int i =0; i<8; i++) {
+				byte temp = (byte) Integer.parseInt(singleByte.substring(i, i+1));
+				tempB += temp << 8-i;
+				
+			}
+			bits.add(tempB);
+			
+		}
+		
+		
+		
+		
+	}
 
 
 	public String Decode(List<Byte> theBits, Map<Character, String> theCodes){
 
-
 		return "";
 	}
 
+	
 	private void countCharFrequency() {
 		int len = textString.length(), curPos = 0;
 		while(len-- > 1) {
-			//			System.out.print(((int) textString.charAt(curPos)) + ": ");
-			//			System.out.println(textString.charAt(curPos));
 			CharF tempCF = new CharF((int)textString.charAt(curPos++));
 			int tempIndex = frequencies.indexOf(tempCF);
 
 			if(tempIndex < 0) frequencies.add(tempCF);
 			else frequencies.get(tempIndex).frequency++; 
-
-
-
 		}
-
-		//		for(int i = 0; i < frequencies.size(); i++ )System.out.println("[ " + i + ", " +frequencies.get(i).ascii + ", " 
-		//				+frequencies.get(i).frequency);
-
 	}
-
-
-
-
 
 	private void generateTree() {
 		while(!frequencies.isEmpty()) {
 			Node temp = new Node(frequencies.remove(0));
-
-			//System.out.println(temp.toString());
-
-
 			nodeQueue.add(temp);
 		}
-
-
-
-
 		Node leftNode, rightNode, parentNode;
 
 		while(nodeQueue.size() > 1) {
 			parentNode = new Node(null);			
 			leftNode = nodeQueue.poll();
-			//System.out.println(leftNode);
 			rightNode = nodeQueue.poll();
-			//System.out.println(rightNode);
 			parentNode.addleft(leftNode);
 			parentNode.addRight(rightNode);
-			//System.out.println(parentNode.toString() + "\n");
 			nodeQueue.add(parentNode);
 		}
 		finishedTree = nodeQueue.poll();
-		
-		//System.out.println("here");
-		//System.out.println(Objects.nonNull(finishedTree.right.right.right.right));
-		//System.out.println(finishedTree.right.left.left.data.ascii);
 	}
-
 
 
 	private void generateCodes() {
-		
 		traverseTree(finishedTree, "");
-
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 	private void traverseTree(Node n, String s) {
 				
 		if(n.isLeaf()) {
-			System.out.println((char)n.data.ascii + " " + s);
 			codes.put(new Character((char)n.data.ascii), s);
 		}else {
-		
-		
-		//if(Objects.isNull(s)) s = "";
-
 		traverseTree(n.left, s += "0");		
 		traverseTree(n.right, s = s.substring(0, s.length()-1) + "1");
 		}
-		
-//		if(Objects.nonNull(n.left)){
-//			s += "0";
-//			traverseTree(n.left, s);
-//		}		
-//		if(Objects.nonNull(n.right)){
-//			s += "1";
-//			traverseTree(n.right, s);
-//		}
-
 	}
 
 
 	public String codeToString() {
-		String result = "";
+		String result = "{";
 		Iterator<Entry<Character, String>> it = codes.entrySet().iterator();
-		Entry<Character, String> tempE = it.next();
-		while(it.hasNext()) result += tempE.getKey() + "=" + tempE.getValue() + ", ";
-		return result;
+		Entry<Character, String> tempE;
+		while(it.hasNext()) {
+			tempE = it.next();
+			result += tempE.getKey() + "=" + tempE.getValue() + ", ";
+		}
+		return result.substring(0, result.length() - 2) + "}";
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 	private class Node implements Comparable<Node>{
@@ -251,9 +172,6 @@ public class CodingTree {
 		}
 	}
 
-
-
-
 	private class CharF {
 		int ascii;
 		int frequency;
@@ -270,8 +188,5 @@ public class CodingTree {
 		public String toString() {
 			return (char)ascii + ", " + frequency;
 		}
-
-
 	}
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintStream;
+import java.math.BigInteger;
+import java.nio.channels.WritableByteChannel;
 import java.util.Scanner;
 
 import javax.swing.JFileChooser;
@@ -38,17 +43,32 @@ public class Main {
 			stream.close();
 		} catch (IOException | NullPointerException e) {
 		
-		}
-		
+		}	
 	}
-	
+	 
 
 	private static void generateOutput(CodingTree textTree) {
 		PrintStream outputFile;
+		
 		try {
-			outputFile = new PrintStream(new File("codes.txt"));
-			outputFile.append(textTree.toString());
+			outputFile = new PrintStream(new File("./codes.txt"));
+			outputFile.append(textTree.codeToString());
+			outputFile.close();
+			
+			//OutputStream outFile = new BufferedOutputStream(new FileOutputStream("./compressed.txt")); 
+			
+			OutputStream out = new BufferedOutputStream(new FileOutputStream("./compressed.txt"));
+			
+			for(int i = 0; i < textTree.bits.size(); i++) {
+				out.write(textTree.bits.get(i).byteValue());
+			}
+			
+			out.close();
+			
 		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
